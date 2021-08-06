7344489bdd23062cd317dbe01210f58d7017ebc2
matias.marchesi03@gmail.com <matias.marchesi03@gmail.com>
2021-08-06 16:23:49 -0300

    Commit 1

---
+++ src/main/java/com/codeoftheweb/salvo/Player.java
@@ -0,0 +1,62 @@
+package com.codeoftheweb.salvo;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toList;
+
+@Entity
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+
+    @OneToMany(mappedBy="playerID", fetch=FetchType.EAGER)
+    Set<GamePlayer> gamePlayers;
+
+    public void addGamePlayer(GamePlayer gameplayer) {
+        gameplayer.setPlayerID(this); //Porque PlayerID?
+        gamePlayers.add(gameplayer);
+    }
+
+    private String userName;
+
+    public Player() { } /*Siempre constructor vacío*/
+
+    public Player(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public Player(Set<GamePlayer> gamePlayers) {
+        this.gamePlayers = gamePlayers;
+    }
+
+    public Set<GamePlayer> getGamePlayers() {
+        return gamePlayers;
+    }
+
+    public void setGamePlayers(Set<GamePlayer> gamePlayers) {
+        this.gamePlayers = gamePlayers;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+
+    public List<Game> getGames() {
+        return gamePlayers.stream().map(sub -> sub.getGameID()).collect(toList());
+    }
+
+
+}
